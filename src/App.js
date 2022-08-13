f57b562934970fdb11fc7492546ca5448cd5a8bd
KatieLam <katiem3749@gmail.com>
2022-08-12 21:23:17 -0400

    experience format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import "./App.scss";
+import Experience from "./components/Experience";
 function App() {
 	return (
 		<div>
 			<Header />
 			<Hero />
+			<Experience />
 		</div>
 	);
 }
