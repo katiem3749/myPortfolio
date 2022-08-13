78df853bab1c4be4240b4a54e5dbd7a74675d727
KatieLam <katiem3749@gmail.com>
2022-08-12 21:17:13 -0400

    header + hero

---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import Kprofile from "../assets/img/Kprofile.jpg";
+import Signature from "../assets/img/signature.png";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Resume from "../assets/Resume_2022-08-01.pdf";
+
+export default function Header() {
+	return (
+		<header id="header" className="header">
+			<div className="d-flex flex-column">
+				{/* profile top + social media links */}
+				<div className="profile">
+					<div className="mb-50">
+						<img
+							src={Kprofile}
+							alt="noreferrence"
+							className="img-fluid rounded-circle"
+						/>
+
+						<a href="index.html">
+							<img
+								src={Signature}
+								className="w-1/2 pt-10 border-0"
+								alt="signature"
+							/>
+						</a>
+					</div>
+
+					<div className="social-links text-center">
+						<a
+							href="https://github.com/katiem3749?tab=repositories"
+							target="_blank"
+							rel="noreferrer">
+							<div className="mt-3 ">
+								<FaGithub className="w-100" />
+							</div>
+						</a>
+
+						<a
+							href="https://www.linkedin.com/in/katie-li-723398105/"
+							target="_blank"
+							rel="noreferrer">
+							<div className=" mt-3">
+								<FaLinkedinIn />
+							</div>
+						</a>
+
+						<a href={Resume}>
+							<div className=" mt-3">
+								<BsFillPersonLinesFill />
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="resume-link">
+					<a href="assets/img/Resume_2022-08-01.pdf">Resume</a>
+				</div>
+			</div>
+		</header>
+	);
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+	return (
+		<section id="hero" class="d-flex flex-column align-items-center ">
+			<div id="hero-container">
+				<h1>
+					Hi, I'm <span>Katie</span>
+				</h1>
+				<p>A Fullstack Developer</p>
+				<p style={{ fontSize: "20px" }}>
+					likes learning & building cool stuff.
+				</p>
+			</div>
+		</section>
+	);
+}
